99f690bd2cfc7ec1b317e82fb1454b4241b36ec0
Laura Hood <laurahood62@gmail.com>
2015-01-20 06:54:21 -0600

    Code from Monday.  Includes smart dashboard selection of autonomous.

--- gary/src/org/usfirst/frc/team5414/robot/Robot.java
+++ gary/src/org/usfirst/frc/team5414/robot/Robot.java
@@ -5,6 +5,8 @@ package org.usfirst.frc.team5414.robot;
 
 import edu.wpi.first.wpilibj.AnalogTrigger;
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -13,7 +15,11 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.buttons.Trigger;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -28,7 +34,8 @@ public class Robot extends IterativeRobot {
 	Joystick joystick1;
 	Joystick joystick2;
 	Trigger trigger1; 
-
+	Command autonomousCommand;
+	SendableChooser autoChooser;
 	int autoLoopCounter;
 	JoystickButton aBtn;
 	JoystickButton bBtn;
@@ -42,6 +49,10 @@ public class Robot extends IterativeRobot {
 	SpeedController driveLeftRearWhite;
 	SpeedController driveRightRearBlue;
 	SpeedController driveLeftFrontBrown;
+	Encoder encoder;
+	Command auton1;
+	Command auton2;
+	CameraServer server;
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -63,8 +74,91 @@ public class Robot extends IterativeRobot {
 		stick = new Joystick(0);
 		joystick1 = new Joystick(1);
 		joystick2 = new Joystick(2);
-		
-		
+		encoder = new Encoder(1, 2, true, EncodingType.k4X);
+		autoChooser = new SendableChooser();
+		auton1=new Command()
+		{
+
+			@Override
+			protected void initialize() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			protected void execute() {
+				// TODO Auto-generated method stub
+				if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+				{
+					encoder.setDistancePerPulse(.5);
+					myRobot.drive(-.75, 0.0); 	// drive forwards half speed
+					autoLoopCounter++;
+				} else {
+					myRobot.drive(0.0, 0.0); 	// stop robot
+				}
+			}
+
+			@Override
+			protected boolean isFinished() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			protected void end() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			protected void interrupted() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		auton2=new Command()
+		{
+
+			@Override
+			protected void initialize() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			protected void execute() {
+				// TODO Auto-generated method stub
+				if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+				{
+					encoder.setDistancePerPulse(.5);
+					myRobot.drive(-.5, 0.5); 	// drive forwards half speed
+					autoLoopCounter++;
+				} else {
+					myRobot.drive(0.0, 0.0); 	// stop robot
+				}
+			}
+
+			@Override
+			protected boolean isFinished() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			protected void end() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			protected void interrupted() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		autoChooser.addDefault("Default program", auton1);
+		autoChooser.addObject("Experimental auto", auton2);
+		SmartDashboard.putData("Autonomous Mode Chooser", autoChooser);
 		aBtn= new JoystickButton(stick, 1);
 		bBtn = new JoystickButton(stick, 2);
 		xBtn = new JoystickButton(stick, 3);
@@ -74,13 +168,11 @@ public class Robot extends IterativeRobot {
 
 		myCompressor = new Compressor(0);
 		DoubleSolenoid1 = new DoubleSolenoid(0,1);
-		/*CameraServer server = CameraServer.getInstance();
+		server = CameraServer.getInstance();
+		server.setSize(1);
 		server.setQuality(50);
 		server.startAutomaticCapture("cam0");
-		CameraServer server2 = CameraServer.getInstance();
-		server2.setQuality(50);
-		server2.startAutomaticCapture("cam1");
-		*/
+		
 	}
 
 	/**
@@ -88,19 +180,19 @@ public class Robot extends IterativeRobot {
 	 */
 	public void autonomousInit() {
 		autoLoopCounter = 0;
+		autonomousCommand = (Command) autoChooser.getSelected();
+		autonomousCommand.start();
 	}
 
 	/**
 	 * This function is called periodically during autonomous
 	 */
 	public void autonomousPeriodic() {
-		if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
-		{
-			myRobot.drive(-.75, 0.0); 	// drive forwards half speed
-			autoLoopCounter++;
-		} else {
-			myRobot.drive(0.0, 0.0); 	// stop robot
-		}
+		Scheduler.getInstance().run();
+		
+		
+			
+	
 	}
 
 	/**
@@ -116,6 +208,7 @@ public class Robot extends IterativeRobot {
 	 */
 	public void teleopPeriodic() {
 		myRobot.tankDrive(stick.getRawAxis(1), stick.getRawAxis(5));
+		
 		//myRobot.tankDrive(stick.getRawAxis(1), stick.getRawAxis(5));
 		//c.start();
 		//c.setClosedLoopControl(true);
